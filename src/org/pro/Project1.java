b4b58d62b44d9a8e8aae947564406bfafb730c14
joynabi <joynabiya.arava398@gmail.com>
2021-09-17 10:33:30 +0530

    project added

---
+++ src/org/pro/Project1.java
@@ -0,0 +1,14 @@
+package org.pro;
+
+public class Project1 {
+	private void tc1() {
+		System.out.println("test1");
+	}
+	private void tc2() {
+		System.out.println("test2");
+
+	}
+	private void tc3() {
+	System.out.println("test3");
+	}
+}
